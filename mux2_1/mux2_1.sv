964e229463487c01f95620cc9bb9c7508ffbd8dd
thinhnguyenhcmut <150126416+Thinhnguyen1006@users.noreply.github.com>
2025-05-21 12:32:41 +0700

    Create mux2_1.sv

---
+++ mux2_1/mux2_1.sv
@@ -0,0 +1,15 @@
+module mux2_1 (
+	input logic [31:0] in0,
+	input logic [31:0] in1,
+	input logic sel,
+	output logic [31:0] out
+);
+
+always @(*) begin
+	case (sel)
+		1'b0:	out = in0;
+		1'b1: out = in1;
+	endcase
+end
+
+endmodule : mux2_1
